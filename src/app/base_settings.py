15ee200b3d0b3024d19ecb7a4d79f1f1d2a5e197
Shevchenko_vlad <Shevchenko.1.vlad@gmail.com>
2018-04-18 16:00:29 +0300

    Added videos

--- src/app/base_settings.py
+++ src/app/base_settings.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'ua'
+LANGUAGE_CODE = 'ru-UA'
 
 TIME_ZONE = 'Europe/Kiev'
 
